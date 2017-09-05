2842bc0a69d846abbc5e9a66a1faca3d68595abf
moxo26 <fmoxo@calstatela.edu>
2017-09-05 14:32:04 -0700

    Added all 8 Sensors into code
    works but it is slow, might consider having node that converts int message into range message

--- arduino/ROS_SR04/ROS_SR04.ino
+++ arduino/ROS_SR04/ROS_SR04.ino
@@ -6,63 +6,77 @@
 
   Modified by Francisco Moxo
   to work with ROS
-  added tf messages that correspond to particular sonar
-  09-02-2017
+  09-05-2017
 */
 #include <ros.h>
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
-//Header for TF broadcast
-#include <tf/transform_broadcaster.h>
-
-
-
-char frameid[] = "base_link";
-
-
-char base_link[] = "/base_link";
-
-
 
 ros::NodeHandle nh;
-
+//Front
 sensor_msgs::Range range_msg1;
 sensor_msgs::Range range_msg2;
-
-geometry_msgs::TransformStamped t1;
-geometry_msgs::TransformStamped t2;
-tf::TransformBroadcaster broadcaster;
-
-// Sensor Topics
-ros::Publisher pub_range1( "/ultrasound1", &range_msg1);
-ros::Publisher pub_range2( "/ultrasound2", &range_msg2);
-//ros::Publisher pub_range3( "/ultrasound3", &range_msg);
-//ros::Publisher pub_range4( "/ultrasound4", &range_msg);
+sensor_msgs::Range range_msg3;
+sensor_msgs::Range range_msg4;
+//Back
+sensor_msgs::Range range_msg5;
+sensor_msgs::Range range_msg6;
+sensor_msgs::Range range_msg7;
+sensor_msgs::Range range_msg8;
 
 
 
+// Sensor Topics Front
+ros::Publisher pub_range1( "/USFL1", &range_msg1);
+ros::Publisher pub_range2( "/USFL2", &range_msg2);
+ros::Publisher pub_range3( "/USFR1", &range_msg3);
+ros::Publisher pub_range4( "/USFR2", &range_msg4);
+
+// Sensor Topics BAck
+ros::Publisher pub_range5( "/USBL1", &range_msg5);
+ros::Publisher pub_range6( "/USBL2", &range_msg6);
+ros::Publisher pub_range7( "/USBR1", &range_msg7);
+ros::Publisher pub_range8( "/USBR2", &range_msg8);
 
 
 
 
 
 
-// defines pins numbers
+
+
+
+// defines pins numbers FRONT
 // Sensor 1
-const int trigPin1 = 12;
-const int echoPin1 = 13;
-
+const int trigPin1 = 30;
+const int echoPin1 = 31;
 // Sensor 2
-const int trigPin2 = 10;
-const int echoPin2 = 11;
-
+const int trigPin2 = 32;
+const int echoPin2 = 33;
 // Sensor 3
-const int trigPin3 = 8;
-const int echoPin3 = 9;
+const int trigPin3 = 34;
+const int echoPin3 = 35;
 
 // Sensor 4
-const int trigPin4 = 6;
-const int echoPin4 = 7;
+const int trigPin4 = 36;
+const int echoPin4 = 37;
+
+
+// defines pins numbers BACK
+// Sensor 5
+const int trigPin5 = 38;
+const int echoPin5 = 39;
+// Sensor 6
+const int trigPin6 = 40;
+const int echoPin6 = 41;
+// Sensor 7
+const int trigPin7 = 42;
+const int echoPin7 = 43;
+// Sensor 8
+const int trigPin8 = 44;
+const int echoPin8 = 45;
+
+
 
 // defines variables
 long duration;
@@ -86,103 +100,142 @@ void setup() {
   pinMode(echoPin4, INPUT); // Sets the echoPin as an Input
 
 
+  //Sensor 5
+  pinMode(trigPin5, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin5, INPUT); // Sets the echoPin as an Input
+
+  //Sensor 6
+  pinMode(trigPin6, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin6, INPUT); // Sets the echoPin as an Input
+
+  //Sensor 7
+  pinMode(trigPin7, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin7, INPUT); // Sets the echoPin as an Input
+
+  //Sensor 8
+  pinMode(trigPin8, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin8, INPUT); // Sets the echoPin as an Input
+
+
 
   nh.initNode();
-  broadcaster.init(nh);
+
 
   // Advertises Node
   nh.advertise(pub_range1);
   nh.advertise(pub_range2);
-  //  nh.advertise(pub_range3);
-  //  nh.advertise(pub_range4);
+  nh.advertise(pub_range3);
+  nh.advertise(pub_range4);
+  nh.advertise(pub_range5);
+  nh.advertise(pub_range6);
+  nh.advertise(pub_range7);
+  nh.advertise(pub_range8);
+
 
   // Sensor 1
   range_msg1.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  range_msg1.header.frame_id =  "/ultrasound1";
-  range_msg1.field_of_view = 0.523;  // fake
+  range_msg1.header.frame_id =  "/USFL1";
+  range_msg1.field_of_view = 0.523;
   range_msg1.min_range = 0.020;
   range_msg1.max_range = 4.00;
 
-  t1.header.frame_id = base_link;
-  t1.child_frame_id = "ultrasound1";
-  t1.transform.translation.x = 2.0;
-  t1.transform.translation.y = -1.0;
-  t1.transform.rotation.x = 0.0;
-  t1.transform.rotation.y = 0.0;
-  t1.transform.rotation.z = -0.7071;
-  t1.transform.rotation.w = 0.7071;
-  
-
-
-
   // Sensor 2
   range_msg2.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  range_msg2.header.frame_id =  "/ultrasound2";
-  range_msg2.field_of_view = 0.523;  // fake
+  range_msg2.header.frame_id =  "/USFL2";
+  range_msg2.field_of_view = 0.523;
   range_msg2.min_range = 0.020;
   range_msg2.max_range = 4.00;
 
-  t2.header.frame_id = base_link;
-  t2.child_frame_id = "/ultrasound2";
-  t2.transform.translation.x = 2.0;
-  t2.transform.translation.y = 1.0;
-  t2.transform.rotation.x = 0.0;
-  t2.transform.rotation.y = 0.0;
-  t2.transform.rotation.z = 0.7071;
-  t2.transform.rotation.w = 0.7071;
+  // Sensor 3
+  range_msg3.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg3.header.frame_id =  "/USFR1";
+  range_msg3.field_of_view = 0.523;
+  range_msg3.min_range = 0.020;
+  range_msg3.max_range = 4.00;
+
+  // Sensor 4
+  range_msg4.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg4.header.frame_id =  "/USFR2";
+  range_msg4.field_of_view = 0.523;
+  range_msg4.min_range = 0.020;
+  range_msg4.max_range = 4.00;
+
+
+  // Sensor 5
+  range_msg5.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg5.header.frame_id =  "/USBL1";
+  range_msg5.field_of_view = 0.523;
+  range_msg5.min_range = 0.020;
+  range_msg5.max_range = 4.00;
+
+  // Sensor 6
+  range_msg6.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg6.header.frame_id =  "/USBL2";
+  range_msg6.field_of_view = 0.523;
+  range_msg6.min_range = 0.020;
+  range_msg6.max_range = 4.00;
+
+  // Sensor 7
+  range_msg7.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg7.header.frame_id =  "/USBR1";
+  range_msg7.field_of_view = 0.523;
+  range_msg7.min_range = 0.020;
+  range_msg7.max_range = 4.00;
+
+  // Sensor 8
+  range_msg8.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg8.header.frame_id =  "/USBR2";
+  range_msg8.field_of_view = 0.523;
+  range_msg8.min_range = 0.020;
+  range_msg8.max_range = 4.00;
+
 
 
 
 }
 
 void loop() {
-
   // Sensor1
-
   range_msg1.range = getRange_Ultrasound(trigPin1, echoPin1);
   range_msg1.header.stamp = nh.now();
-  t1.header.stamp = nh.now();
-  broadcaster.sendTransform(t1);
   pub_range1.publish( &range_msg1);
 
-
-
-
   // Sensor2
-
   range_msg2.range = getRange_Ultrasound(trigPin2, echoPin2);
   range_msg2.header.stamp = nh.now();
-  t2.header.stamp = nh.now();
-  broadcaster.sendTransform(t2);
   pub_range2.publish( &range_msg2);
 
+  // Sensor3
+  range_msg3.range = getRange_Ultrasound(trigPin3, echoPin3);
+  range_msg3.header.stamp = nh.now();
+  pub_range3.publish( &range_msg3);
 
-  //
-  //  // Sensor3
-  //  range_msg.range = getRange_Ultrasound(trigPin3, echoPin3);
-  //  //  range_msg.header.frame_id =  "/ultrasound3";
-  //  range_msg.header.stamp = nh.now();
-  //  pub_range3.publish( &range_msg);
-  //  t.header.frame_id = "/ultrasound3";
-  //  t.child_frame_id = base_link;
-  //  t.transform.translation.x = 1.0;
-  //  t.transform.translation.y = -2.0;
-  //  t.transform.rotation.x = 0.0;
-  //  t.transform.rotation.y = 0.0;
-  //  t.transform.rotation.z = 0.0;
-  //  t.transform.rotation.w = 1.0;
-  //  t.header.stamp = nh.now();
-  //  broadcaster.sendTransform(t);
+  // Sensor4
+  range_msg4.range = getRange_Ultrasound(trigPin4, echoPin4);
+  range_msg4.header.stamp = nh.now();
+  pub_range4.publish( &range_msg4);
 
+  nh.spinOnce();
 
+  // Sensor5
+  range_msg5.range = getRange_Ultrasound(trigPin5, echoPin5);
+  range_msg5.header.stamp = nh.now();
+  pub_range5.publish( &range_msg5);
 
-  //
-  //  // Sensor4
-  //  range_msg.range = getRange_Ultrasound(trigPin4, echoPin4);
-  //  //  range_msg.header.frame_id =  "/ultrasound4";
-  //  range_msg.header.stamp = nh.now();
-  //  pub_range4.publish( &range_msg);
+  // Sensor6
+  range_msg6.range = getRange_Ultrasound(trigPin6, echoPin6);
+  range_msg6.header.stamp = nh.now();
+  pub_range6.publish( &range_msg6);
 
+  // Sensor7
+  range_msg7.range = getRange_Ultrasound(trigPin7, echoPin7);
+  range_msg7.header.stamp = nh.now();
+  pub_range7.publish( &range_msg7);
+
+  // Sensor8
+  range_msg8.range = getRange_Ultrasound(trigPin8, echoPin8);
+  range_msg8.header.stamp = nh.now();
+  pub_range8.publish( &range_msg8);
 
 
   nh.spinOnce();
@@ -205,5 +258,5 @@ float getRange_Ultrasound(int trigPin, int echoPin) {
 
   // Calculating the distance
   distance = duration * 0.034 / 2;
-  return distance/100 ; //in meters
+  return distance / 100 ; //in meters
 }
