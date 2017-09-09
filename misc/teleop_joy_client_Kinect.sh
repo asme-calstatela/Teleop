4447d96dcbb69297565ed0bc46af90aa47cabcdd
jreyes81 <jeovannyreyes46@yahoo.com>
2017-09-09 12:49:26 -0700

    Updated teleop_joy_client_Kinect.sh file
    Commented out rqt gui and moved it after launching the teleop roslaunch file

--- misc/teleop_joy_client_Kinect.sh
+++ misc/teleop_joy_client_Kinect.sh
@@ -5,12 +5,12 @@
 #The address might be different so do hostname -I on your computer
 #export ROS_IP='192.168.1.198'
 
-# Activate the rqt GUI for The Teleoperation
-echo "Launching Teleoperation GUI perspective"
-rqt --perspective-file Teleoperation.perspective
-
- rviz rviz -f base_link
-
 # Launching ROS Packages
 roslaunch teleop teleop_joy_client_Kinect.launch
 # Include package for ultrasounds, IMU, etc.
+
+# Activate the rqt GUI for The Teleoperation
+#echo "Launching Teleoperation GUI perspective"
+#rqt --perspective-file Teleoperation.perspective
+
+#rviz rviz -f base_link
