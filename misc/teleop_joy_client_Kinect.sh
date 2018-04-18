11905cf7d71b634563d16f4f5e5fb80d42414984
jreyes81 <jreyes81@calstatela.edu>
2018-04-18 12:27:46 -0700

    files in the misc folder

--- misc/teleop_joy_client_Kinect.sh
+++ misc/teleop_joy_client_Kinect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #This script is for the client_Kinect
 
-#export ROS_MASTER_URI='http://192.168.1.193:11311'
+export ROS_MASTER_URI='http://192.168.1.193:11311'
 #The address might be different so do hostname -I on your computer
-#export ROS_IP='192.168.1.198'
+export ROS_IP='192.168.1.173' # This is the IP address for the desktop in the lab!
 
 # Launching ROS Packages
 roslaunch teleop teleop_joy_client_Kinect.launch
--- misc/teleop_joy_server_Kinect.sh
+++ misc/teleop_joy_server_Kinect.sh
@@ -2,6 +2,6 @@
 #This script is for the server_Kinect
 
 # Put this in ~/.bashrc file by doing $ gedit ~/.bashrc
-#export ROS_IP=192.168.1.193
+export ROS_IP=192.168.1.193
 
 roslaunch teleop teleop_joy_server_Kinect.launch
