928bcc01d8e99edde7eab97bd9fdc7f30a6e02d3
jreyes81 <jreyes81@calstatela.edu>
2018-05-24 17:52:00 -0700

    Robot only moves when trigger is held

--- arduino/differential_drive/differential_drive.ino
+++ arduino/differential_drive/differential_drive.ino
@@ -72,7 +72,7 @@ Servo servo_right;
 int state_run = 0; // Variable to tell if trigger is pressed
 
 
-void joy_cb(const sensor_msgs::Joy& joy){
+void joydata(const sensor_msgs::Joy& joy){
   int hold_buttval = joy.buttons[0]; 
   if (joy.buttons[0] == 1 && joy.buttons[1] == 0 && joy.buttons[3] == 0) {
      state_run = 1; // This will activate to get robot to move
@@ -95,13 +95,13 @@ void servo_cb(const geometry_msgs::Twist& cmd_msg){
       direction_left = int ( STOP * (1 - LEFT_SCALE * linear) );
       direction_right = int ( STOP * (1 + RIGHT_SCALE * linear) );
     
-      Serial.print("Translate\n");
+//      Serial.print("Translate\n");/
    } else if (angular > THRESHOLD || angular < -THRESHOLD) {
     // Rotate counter clockwise
       direction_left = int ( STOP * (1 + LEFT_SCALE * angular) );
       direction_right = int ( STOP * (1 + RIGHT_SCALE * angular) );
     
-      Serial.print("Rotate\n");
+//      Serial.print("Rotate\n");/
     } else {
       // STOP!
 
@@ -109,24 +109,27 @@ void servo_cb(const geometry_msgs::Twist& cmd_msg){
       direction_left = STOP;
       direction_right = STOP;
     
-      Serial.print("Stop\n");
-    }
-
-   if (state_run == 0){
-    direction_left = STOP;
-    direction_right = STOP;
+//      Serial.print/("Stop\n");
     }
 
   servo_left.write(direction_left); //set servo_left angle
   servo_right.write(direction_right); //set servo_right angle
   }
+  else {
+    direction_left = STOP;
+    direction_right = STOP;
+    servo_left.write(direction_left); //set servo_left angle
+    servo_right.write(direction_right); //set servo_right angle
+    }
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub_cmd_vel("cmd_vel", servo_cb);
+ros::Subscriber<sensor_msgs::Joy> sub_joy_button("joy", joydata);
 
 void setup(){
   nh.initNode();
   nh.subscribe(sub_cmd_vel);
+  nh.subscribe(sub_joy_button);
 
   servo_left.attach(8); //attach it to pin 8
   servo_right.attach(9); //attach it to pin 9
