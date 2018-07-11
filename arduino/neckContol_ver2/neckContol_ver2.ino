1f3412b5fa67bbee7b7bb100a01138efa02240cc
jreyes81 <jreyes81@calstatela.edu>
2018-07-10 17:12:05 -0700

    Added third servo to control

--- arduino/neckContol_ver2/neckContol_ver2.ino
+++ arduino/neckContol_ver2/neckContol_ver2.ino
@@ -27,16 +27,22 @@ Servo servo1, servo2, servo3;
 
 int pos1 = 90;
 int pos2 = 90;
-//int pos3 = 90;
+int pos3 = 90;
+
 int new_pos1 = 90;
 int new_pos2 = 90;
+int new_pos3 = 90;
+
 int old_pos1 = 90;
 int old_pos2 = 90;
+int old_pos3 = 90;
 
 void joydata(const sensor_msgs::Joy& joy){
-  int joy_butt = joy.buttons[2];
+  int joy_butt = joy.buttons[2]; // Trigger to move servos 1 and 2
   float joy_fb = joy.axes[1]; // forward and backward
   float joy_lr = joy.axes[0]; // left and right
+  float joy_servo3 = joy.axes[4]; // small black joystick. Up is +1
+  
   if (joy_butt == 1){
        new_pos1 = pos1 + (pos1*joy_fb);
        new_pos2 = pos2 + (pos2*joy_lr);
@@ -53,9 +59,18 @@ void joydata(const sensor_msgs::Joy& joy){
        old_pos1 = new_pos1;
        old_pos2 = new_pos2;
   }
+
+  else if(joy_servo3 > 0){
+      new_pos3 = pos3 + (pos3*joy_fb);
+
+      servo3.write(new_pos3);
+
+      old_pos3 = new_pos3;
+  }
   else {
      servo1.write(old_pos1);
      servo2.write(old_pos2);
+     servo3.write(old_pos3);
 
      first_servo_msg.data = new_pos1;
      second_servo_msg.data = new_pos2;
@@ -76,8 +91,10 @@ nh.advertise(servo_pos2);
 
 servo1.attach(9, 500, 2500);
 servo2.attach(10, 500, 2500);
-servo1.write(90);
-servo2.write(90);
+servo3.attach(11, 500, 2500);
+servo1.write(pos1);
+servo2.write(pos2);
+servo3.write(pos3);
 }
 
 void loop() {
